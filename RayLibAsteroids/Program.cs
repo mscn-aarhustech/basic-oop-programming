fc882356f4910f6db876697c11bd12a907e22045
Michael Schmidt Nissen <mscn@aarhustech.dk>
2025-11-19 15:28:27 +0100

    added raylib test game

---
+++ RayLibAsteroids/Program.cs
@@ -0,0 +1,27 @@
+﻿using Raylib_cs;
+
+namespace MyRaylibGame;
+
+class Program
+{
+    static void Main()
+    {
+        Raylib.InitWindow(800, 480, "Hello World");
+
+        while (!Raylib.WindowShouldClose())
+        {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.White);
+
+            Raylib.DrawCircle(400, 240, 50, Color.Red);
+
+            Raylib.DrawLine(100, 100, 200, 200, Color.Blue);
+
+            Raylib.DrawText("Setup Successful!", 190, 200, 20, Color.LightGray);
+
+            Raylib.EndDrawing();
+        }
+
+        Raylib.CloseWindow();
+    }
+}
