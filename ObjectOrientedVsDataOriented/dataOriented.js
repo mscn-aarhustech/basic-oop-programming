8cb7377f76fcfc0f6ab781f817ce236eb447eb2f
Michael Schmidt Nissen <mscn@aarhustech.dk>
2026-02-24 14:23:18 +0100

    object oriented vs data oriented javascript example

---
+++ ObjectOrientedVsDataOriented/dataOriented.js
@@ -0,0 +1,117 @@
+
+class Planets {
+    constructor(maxObjects) {
+        
+        this.maxObjects = maxObjects;
+
+        this.positionX = new Float32Array(maxObjects);
+        this.positionY = new Float32Array(maxObjects);
+        this.velocityX = new Float32Array(maxObjects);
+        this.velocityY = new Float32Array(maxObjects);
+        this.forceX = new Float32Array(maxObjects);
+        this.forceY = new Float32Array(maxObjects);
+        this.mass = new Float32Array(maxObjects);
+        this.density = new Float32Array(maxObjects);
+        this.radius = new Float32Array(maxObjects);
+        this.color = new Uint8Array(maxObjects * 3);
+    }
+}
+
+class World {
+    constructor(width, height, numPlanets) {
+        this.width = width;
+        this.height = height;
+        this.gravitationalConst = 10.0;
+        this.timeStep = 0.01;
+        this.planets = new Planets(numPlanets);
+
+        for(let i = 0; i < numPlanets; i++) {
+            this.planets.positionX[i] = Math.random() * this.width;
+            this.planets.positionY[i] = Math.random() * this.height;
+            this.planets.mass[i] = 10 + Math.random() * 90;
+            this.planets.density[i] = 0.5;
+            this.planets.radius[i] = Math.sqrt(this.planets.mass[i] / (this.planets.density[i] * Math.PI));
+            const r = Math.floor(Math.random() * 256);
+            const g = Math.floor(Math.random() * 256);
+            const b = Math.floor(Math.random() * 256);
+            this.planets.color[i * 3] = r;
+            this.planets.color[i * 3 + 1] = g;
+            this.planets.color[i * 3 + 2] = b;
+        }
+    }
+    run() {
+        this.calculateGravity();
+        this.integrate();
+        //this.render();
+        //requestAnimationFrame(() => this.run());
+    }
+    calculateGravity() {
+        for(let i = 0; i < this.planets.maxObjects - 1; i++) {
+            for(let j = i + 1; j < this.planets.maxObjects; j++) {
+
+                const distanceX = this.planets.positionX[j] - this.planets.positionX[i];
+                const distanceY = this.planets.positionY[j] - this.planets.positionY[i];
+
+                const distanceSquared = distanceX * distanceX + distanceY * distanceY;
+
+                const sumRadiiSquared = (this.planets.radius[i] + this.planets.radius[j]) ** 2;
+
+                if(distanceSquared < sumRadiiSquared) { continue };
+
+                const distance = Math.sqrt(distanceSquared);
+
+                const unitX = distanceX / distance;
+                const unitY = distanceY / distance;
+
+                const gravityForce = (this.gravitationalConst * this.planets.mass[i] * this.planets.mass[j]) / distanceSquared;
+
+                this.planets.forceX[i] += gravityForce * unitX;
+                this.planets.forceY[i] += gravityForce * unitY;
+                this.planets.forceX[j] -= gravityForce * unitX;
+                this.planets.forceY[j] -= gravityForce * unitY;
+            }
+        }
+    }
+    integrate() {
+        for(let i = 0; i < this.planets.maxObjects; i++) {
+
+            this.planets.velocityX[i] += (this.planets.forceX[i] / this.planets.mass[i]) * this.timeStep;
+            this.planets.velocityY[i] += (this.planets.forceY[i] / this.planets.mass[i]) * this.timeStep;
+
+            this.planets.positionX[i] += this.planets.velocityX[i] * this.timeStep;
+            this.planets.positionY[i] += this.planets.velocityY[i] * this.timeStep;
+
+            this.planets.forceX[i] = 0.0;
+            this.planets.forceY[i] = 0.0;
+        }
+    }
+    render() {
+        
+        // Clear screen
+        context.resetTransform();
+        context.fillStyle = "rgb(0,0,0)";
+        context.fillRect(0, 0, canvas.width, canvas.height);
+
+        // Render planets
+        for(let i = 0; i < this.planets.maxObjects; i++) {
+            context.fillStyle = `rgb(${this.planets.color[i * 3]}, ${this.planets.color[i * 3 + 1]}, ${this.planets.color[i * 3 + 2]})`;
+            context.beginPath();
+			context.arc(this.planets.positionX[i], this.planets.positionY[i], this.planets.radius[i], 0, Math.PI * 2);
+			context.fill();
+			context.closePath();
+        }
+
+        requestAnimationFrame(() => this.render());
+    }
+}
+
+let canvas = document.querySelector("#simulationCanvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext("2d");
+
+const world = new World(canvas.width, canvas.height, 1000);
+console.log(world);
+
+requestAnimationFrame(() => world.render());
+setInterval(() => world.run(), 0);
---
+++ ObjectOrientedVsDataOriented/objectOriented.js
@@ -0,0 +1,129 @@
+class Vector2 {
+    constructor(x = 0.0, y = 0.0) {
+        this.x = x;
+        this.y = y;
+    }
+    add(v) {
+        return new Vector2(this.x + v.x, this.y + v.y);
+    }
+    subtract(v)  {
+        return new Vector2(this.x - v.x, this.y - v.y);
+    }
+    multiply(s) {
+        return new Vector2(this.x * s, this.y * s);
+    }
+    divide(s) {
+        return new Vector2(this.x / s, this.y / s);
+    }
+}
+
+class Planet {
+    constructor(posx, posy, mass = 1.0) {
+       this.position = new Vector2(posx, posy);
+       this.velocity = new Vector2();
+       this.force = new Vector2();
+       this.mass = mass;
+       this.density = 0.5;
+       this.radius = Math.sqrt(this.mass / (this.density * Math.PI));
+       const r = Math.floor(Math.random() * 256);
+       const g = Math.floor(Math.random() * 256);
+       const b = Math.floor(Math.random() * 256);
+       this.color = `rgb(${r}, ${g}, ${b})`;
+
+       // Add dummy properties to bloat the class
+       // This prevents a class instances from fitting into the cache and forces heap allocation
+        for (let i = 0; i < 100; i++) {
+            this[`dummy_property_${i}`] = Math.random();
+        }
+    }
+}
+
+class World {
+    constructor(width, height, numPlanets) {
+        this.width = width;
+        this.height = height;
+        this.gravitationalConst = 10.0;
+        this.timeStep = 0.01;
+        this.planets = [];
+        for(let p = 0; p < numPlanets; p++) {
+            const planet = new Planet(Math.random() * this.width, Math.random() * this.height, 10 + Math.random() * 90);
+            this.planets.push(planet);
+        }
+    }
+    run() {
+        this.calculateGravity();
+        this.integrate();
+        //this.render();
+        //requestAnimationFrame(() => this.run());
+    }
+    calculateGravity() {
+        for(let i = 0; i < this.planets.length - 1; i++) {
+            for(let j = i + 1; j < this.planets.length; j++) {
+                const planet_i = this.planets[i];
+                const planet_j = this.planets[j];
+                
+                const distanceVector = new Vector2(
+                    planet_j.position.x - planet_i.position.x, 
+                    planet_j.position.y - planet_i.position.y
+                );
+
+                const distanceSquared = distanceVector.x * distanceVector.x + distanceVector.y * distanceVector.y;
+
+                const sumRadiiSquared = (planet_i.radius + planet_j.radius) ** 2;
+
+                if(distanceSquared < sumRadiiSquared) { continue };
+
+                const distance = Math.sqrt(distanceSquared);
+
+                const unitVector = distanceVector.divide(distance);
+
+                const gravityForce = (this.gravitationalConst * planet_i.mass * planet_j.mass) / distanceSquared;
+
+                const forceVector = unitVector.multiply(gravityForce);
+
+                planet_i.force = planet_i.force.add(forceVector);
+                planet_j.force = planet_j.force.subtract(forceVector);
+            }
+        }
+    }
+    integrate() {
+        for(let i = 0; i < this.planets.length; i++) {
+            const planet = this.planets[i];
+
+            planet.velocity = planet.velocity.add(planet.force.divide(planet.mass).multiply(this.timeStep));
+            planet.position = planet.position.add(planet.velocity.multiply(this.timeStep));
+
+            planet.force = new Vector2();
+        }
+    }
+    render() {
+        // Clear screen
+        context.resetTransform();
+        //ctx.fillStyle = "rgb(32,96,32)";
+        context.fillStyle = "rgb(0,0,0)";
+        context.fillRect(0, 0, canvas.width, canvas.height);
+
+        for(let i = 0; i < this.planets.length; i++) {
+            const planet = this.planets[i];
+            context.fillStyle = planet.color;
+            context.beginPath();
+			context.arc(planet.position.x, planet.position.y, planet.radius, 0, Math.PI * 2);
+			context.fill();
+			context.closePath();
+        }
+
+        requestAnimationFrame(() => this.render());
+    }
+}
+
+let canvas = document.querySelector("#simulationCanvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext("2d");
+
+const world = new World(canvas.width, canvas.height, 1000);
+
+console.log(world);
+
+requestAnimationFrame(() => world.render());
+setInterval(() => world.run(), 0);
